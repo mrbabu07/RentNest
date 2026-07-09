63f05ad43be09ca7e474198afb7e7f11e5a90c9d
mrbabu07 <mdjahedulislamjaved@gmail.com>
2026-07-09 11:39:39 +0600

    updated admin controller

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,37 +1,33 @@
-import { Request, Response } from "express";
-import catchAsync from "../../utils/catchAsync";
-import * as AdminService from './admin.service';
-import { success } from "zod";
-
+import { Request, Response } from 'express';
+import catchAsync from '../../utils/catchAsync';
+import * as adminService from './admin.service';
 
 export const getAllUsers = catchAsync(async (req: Request, res: Response) => {
-    const users = await AdminService.getAllUsers();
-    res.status(200).json({success: true, data: users});
-
-})
+  const users = await adminService.getAllUsers();
+  res.status(200).json({ success: true, data: users });
+});
 
 export const updateUserStatus = catchAsync(async (req: Request, res: Response) => {
-    const {status } = req.body;
-    const user = await AdminService.updateUserStatus(req.params.id, status);
-    res.status(200).json({
-        success: true,
-        message: `User ${status === 'BANNED' ? 'banned': 'unbanned'} successfully`,
-        data: user,
-    });
+  const { status } = req.body;
+  const user = await adminService.updateUserStatus(req.params.id, status);
+  res.status(200).json({
+    success: true,
+    message: `User ${status === 'BANNED' ? 'banned' : 'unbanned'} successfully`,
+    data: user,
+  });
 });
 
-export const getAllProperties = catchAsync(async (req: Request, res: Response) =>{
-    const properties = await AdminService.getAllPropertiesAdmin();
-    res.status(200).json({success: true, data: properties});
+export const getAllProperties = catchAsync(async (req: Request, res: Response) => {
+  const properties = await adminService.getAllPropertiesAdmin();
+  res.status(200).json({ success: true, data: properties });
 });
 
-export const getAllRentalRequests = catchAsync(async(req: Request, res: Response) =>{
-    const requests = await AdminService.getAllRentalRequestsAdmin();
-    res.status(200).json({success: true, data: requests});
+export const getAllRentalRequests = catchAsync(async (req: Request, res: Response) => {
+  const requests = await adminService.getAllRentalRequestsAdmin();
+  res.status(200).json({ success: true, data: requests });
+});
 
-})
-
-export const getDashboardStats = catchAsync(async(req: Request, res: Response)=> {
-    const stats = await AdminService.getDashboardStats();
-    res.status(200).json({success: true, data: stats});;
+export const getDashboardStats = catchAsync(async (req: Request, res: Response) => {
+  const stats = await adminService.getDashboardStats();
+  res.status(200).json({ success: true, data: stats });
 });
