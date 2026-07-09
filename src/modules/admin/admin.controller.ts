e9d3385db6382ae8e1bfcd31b82a61839f4bd93b
mrbabu07 <mdjahedulislamjaved@gmail.com>
2026-07-09 11:31:04 +0600

    updated userstatus

---
+++ src/modules/admin/admin.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import catchAsync from "../../utils/catchAsync";
+import * as AdminService from './admin.service';
+import { success } from "zod";
+
+
+export const getAllUsers = catchAsync(async (req: Request, res: Response) => {
+    const users = await AdminService.getAllUsers();
+    res.status(200).json({success: true, data: users});
+
+})
+
+export const updateUserStatus = catchAsync(async (req: Request, res: Response) => {
+    const {status } = req.body;
+    const user = await AdminService.updateUserStatus(req.params.id, status);
+    res.status(200).json({
+        success: true,
+        message: `User ${status === 'BANNED' ? 'banned': 'unbanned'} successfully`,
+        data: user,
+    });
+});
